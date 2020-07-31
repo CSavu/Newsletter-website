33e8995a318bcd3f8105356905e0f128a436d9cf
CSavu <66332005+CSavu@users.noreply.github.com>
2020-07-31 10:30:36 +0300

    added npms

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+})
